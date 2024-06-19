9a761276265f0bdb642782a8701663663ab2528c
bellashrns <bella.saharani@student.umn.ac.id>
2024-06-19 11:36:08 +0700

    remove unnecessary file

--- app/src/main/java/com/example/alfaresto_customersapp/domain/repository/MenuRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alfaresto_customersapp.domain.repository
-
-import androidx.paging.PagingData
-import com.example.alfaresto_customersapp.domain.model.Menu
-import kotlinx.coroutines.flow.Flow
-
-interface MenuRepository {
-    fun getMenus(): Flow<PagingData<Menu>>
-}
