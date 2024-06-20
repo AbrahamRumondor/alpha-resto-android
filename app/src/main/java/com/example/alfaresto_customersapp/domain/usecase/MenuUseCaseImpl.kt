82033e2fe600f590ad047e1142b409e59d363b02
bellashrns <bella.saharani@student.umn.ac.id>
2024-06-20 09:17:42 +0700

    delete Log

--- app/src/main/java/com/example/alfaresto_customersapp/domain/usecase/MenuUseCaseImpl.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/domain/usecase/MenuUseCaseImpl.kt
@@ -1,6 +1,5 @@
 package com.example.alfaresto_customersapp.domain.usecase
 
-import android.util.Log
 import com.example.alfaresto_customersapp.domain.model.Menu
 import com.example.alfaresto_customersapp.domain.repository.MenuRepository
 import kotlinx.coroutines.flow.StateFlow
@@ -11,8 +10,6 @@ class MenuUseCaseImpl @Inject constructor(
 ) : MenuUseCase {
 
     override suspend fun getMenus(): StateFlow<List<Menu>> {
-        val fetchedMenus = menuRepository.getMenus()
-        Log.d("MENU usecaseImpl", "Menus fetched: ${fetchedMenus.value}")
-        return fetchedMenus
+        return menuRepository.getMenus()
     }
 }
