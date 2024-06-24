0be6603fead7da4d1d17b108d8219fc3af4768b0
abrahamrumondor <intern.abraham@gli.id>
2024-06-24 08:08:04 +0700

    model updates

--- app/src/main/java/com/example/alfaresto_customersapp/domain/model/Order.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/domain/model/Order.kt
@@ -1,12 +1,35 @@
 package com.example.alfaresto_customersapp.domain.model
 
+import com.google.firebase.firestore.PropertyName
+
 data class Order(
-    val id: String = "",
-    val userID: String = "",
-    val addressID: String = "",
-    val restoID: String = "",
-    val date : String = "",
-    val paymentMethod : String = "",
-    val totalPrice : Int = -1,
-    val items: List<OrderItem> = mutableListOf()
+    @get:PropertyName("order_id")
+    @set:PropertyName("order_id")
+    var id: String = "",
+
+    @get:PropertyName("user_id")
+    @set:PropertyName("user_id")
+    var userID: String = "",
+
+    @get:PropertyName("address_id")
+    @set:PropertyName("address_id")
+    var addressID: String = "",
+
+    @get:PropertyName("resto_id")
+    @set:PropertyName("resto_id")
+    var restoID: String = "",
+
+    @get:PropertyName("date")
+    @set:PropertyName("date")
+    var date : String = "",
+
+    @get:PropertyName("payment_method")
+    @set:PropertyName("payment_method")
+    var paymentMethod : String = "",
+
+    @get:PropertyName("total_price")
+    @set:PropertyName("total_price")
+    var totalPrice : Int = -1,
+
+    var items: List<OrderItem> = mutableListOf()
 )
--- app/src/main/java/com/example/alfaresto_customersapp/domain/model/OrderItem.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/domain/model/OrderItem.kt
@@ -1,8 +1,21 @@
 package com.example.alfaresto_customersapp.domain.model
 
+import com.google.firebase.firestore.PropertyName
+
 data class OrderItem(
-    val id: String = "",
-    val menuID: String = "",
-    val quantity: Int = -1,
-    val menuPrice: Int = -1,
+    @get:PropertyName("order_item_id")
+    @set:PropertyName("order_item_id")
+    var id: String = "",
+
+    @get:PropertyName("menu_id")
+    @set:PropertyName("menu_id")
+    var menuID: String = "",
+
+    @get:PropertyName("quantity")
+    @set:PropertyName("v")
+    var quantity: Int = -1,
+
+    @get:PropertyName("menu_price")
+    @set:PropertyName("menu_price")
+    var menuPrice: Int = -1,
 )
