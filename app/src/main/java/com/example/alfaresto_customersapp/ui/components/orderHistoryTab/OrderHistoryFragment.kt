276d9a5686dd80ab14313393c356f0fd1915ad1f
kelshaaira <kelshairaa@gmail.com>
2024-06-26 16:55:54 +0700

    History detail layout

--- app/src/main/java/com/example/alfaresto_customersapp/ui/components/orderHistoryTab/OrderHistoryFragment.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/orderHistoryTab/OrderHistoryFragment.kt
@@ -1,6 +1,5 @@
 package com.example.alfaresto_customersapp.ui.components.orderHistoryTab
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
