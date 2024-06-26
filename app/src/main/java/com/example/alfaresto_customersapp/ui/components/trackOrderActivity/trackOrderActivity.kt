ad236ec7c30fb0698dcf714b73339eac61300e01
kelshaaira <kelshairaa@gmail.com>
2024-06-26 09:05:00 +0700

    add track order activity

---
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/trackOrderActivity/trackOrderActivity.kt
@@ -0,0 +1,24 @@
+package com.example.alfaresto_customersapp.ui.components.trackOrderActivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+//import com.example.alfaresto_customersapp.databinding.ActivityTrackBinding
+//import com.example.alfaresto_customersapp.ui.components.chatActivity.ChatActivity
+
+class TrackOrderActivity : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityTrackBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //binding = ActivityTrackBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.chatButton.setOnClickListener {
+//            val intent = Intent(this, ChatActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
