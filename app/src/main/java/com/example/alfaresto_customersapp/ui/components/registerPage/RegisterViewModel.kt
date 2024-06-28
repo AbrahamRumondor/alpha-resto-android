3f304e01b7248d29c1d9827e6806262b233370ed
kelshaaira <kelshairaa@gmail.com>
2024-06-28 17:08:50 +0700

    solve conflict

--- app/src/main/java/com/example/alfaresto_customersapp/ui/components/registerPage/RegisterViewModel.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/registerPage/RegisterViewModel.kt
@@ -11,14 +11,14 @@ class RegisterViewModel : ViewModel() {
     private val auth = FirebaseAuth.getInstance()
     private val firestore = FirebaseFirestore.getInstance()
 
-    fun registerUser(email: String, image: String, name: String, noTelp: String, password: String, onComplete: (Boolean) -> Unit) {
+    fun registerUser(email: String, image: String, name: String, phone: String, password: String, onComplete: (Boolean) -> Unit) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { register ->
                 if (register.isSuccessful) {
                     val user = auth.currentUser
                     val id = user?.uid ?: return@addOnCompleteListener
                     val hashedPassword = hashPassword(password)
-                    val newUser = User(email, id, image, name, noTelp, hashedPassword)
+                    val newUser = User(email, id, image, name, phone, hashedPassword)
                     addToFirestore(newUser) { success ->
                         onComplete(success)
                     }
@@ -30,12 +30,11 @@ class RegisterViewModel : ViewModel() {
 
     private fun addToFirestore(user: User, onComplete: (Boolean) -> Unit) {
         val userMap = hashMapOf(
-            "user_email" to user.userEmail,
-            "user_id" to user.userId,
-            "user_image" to user.userImage,
-            "user_name" to user.userName,
-            "user_no_telp" to user.userNoTelp,
-            "user_password" to user.userPassword
+            "user_email" to user.email,
+            "user_id" to user.id,
+            "user_name" to user.name,
+            "user_no_telp" to user.phone,
+            "user_password" to user.password
         )
 
         firestore.collection("users").document(user.userId)
