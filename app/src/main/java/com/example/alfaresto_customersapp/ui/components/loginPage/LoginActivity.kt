32e8afc63b4f1f48a4c295362dfc0ae9ea962dc9
kelshaaira <kelshairaa@gmail.com>
2024-06-29 21:18:36 +0700

    Add shared preference to handle user login

--- app/src/main/java/com/example/alfaresto_customersapp/ui/components/loginPage/LoginActivity.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/loginPage/LoginActivity.kt
@@ -23,6 +23,11 @@ class LoginActivity : AppCompatActivity() {
         binding = LoginPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (isLoggedIn()) {
+            navigateToMainActivity()
+            return
+        }
+
         val authRepository = AuthRepositoryImpl()
         viewModel = LoginViewModel(authRepository)
 
@@ -33,8 +38,8 @@ class LoginActivity : AppCompatActivity() {
         viewModel.loginResult.observe(this) { success ->
             if (success) {
                 Toast.makeText(this, R.string.login_success, Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                saveLoginStatus(true)
+                navigateToMainActivity()
             } else {
                 Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show()
             }
@@ -67,6 +72,25 @@ class LoginActivity : AppCompatActivity() {
     private fun goToRegisterPage(view: View) {
         val intent = Intent(view.context.applicationContext, RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun saveLoginStatus(isLoggedIn: Boolean) {
+        val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("isLoggedIn", isLoggedIn)
+            apply()
+        }
+    }
+
+    private fun isLoggedIn(): Boolean {
+        val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
+        return sharedPreferences.getBoolean("isLoggedIn", false)
     }
 }
 
--- app/src/main/java/com/example/alfaresto_customersapp/ui/components/restoTab/RestoFragment.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/restoTab/RestoFragment.kt
@@ -1,16 +1,20 @@
 package com.example.alfaresto_customersapp.ui.components.restoTab
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.alfaresto_customersapp.R
 import com.example.alfaresto_customersapp.databinding.FragmentRestoBinding
+import com.example.alfaresto_customersapp.ui.components.loginPage.LoginActivity
 import com.example.alfaresto_customersapp.ui.components.restoTab.adapter.RestoAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -51,5 +55,32 @@ class RestoFragment : Fragment() {
             Navigation.findNavController(view)
                 .navigate(R.id.action_resto_fragment_to_list_all_menu_fragment)
         }
+
+        binding.toolbar.btnLogout.setOnClickListener {
+            logoutValidation()
+        }
+    }
+
+    private fun logoutValidation() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Confirmation")
+            .setMessage("Are you sure you want to logout?")
+            .setPositiveButton("Logout") { _, _ ->
+                logout()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun logout() {
+        val sharedPreferences = requireContext().getSharedPreferences("login", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("isLoggedIn", false)
+            apply()
+        }
+
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
