706bec7ce7254eeb6c9ddc6b490640713a5e04ec
abrahamrumondor <intern.abraham@gli.id>
2024-07-01 09:58:01 +0700

    order history list observes update

--- app/src/main/java/com/example/alfaresto_customersapp/data/di/ShipmentRepositoryImpl.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/data/di/ShipmentRepositoryImpl.kt
@@ -32,13 +32,20 @@ class ShipmentRepositoryImpl @Inject constructor(
     private var listenerRegistration: ListenerRegistration? = null
 
     override suspend fun getShipments(): LiveData<List<Shipment>> {
-        try {
-            val snapshot = shipmentsRef.get().await()
-            val shipmentList = snapshot.toObjects(ShipmentResponse::class.java)
-                .map { ShipmentResponse.transform(it) }
-            _shipments.value = shipmentList
-        } catch (e: Exception) {
-            _shipments.value = emptyList()
+        shipmentsRef.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                Log.e("Firestore", "Error fetching documents: $error")
+                _shipments.value = emptyList()
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null) {
+                val shipmentList = snapshot.toObjects(ShipmentResponse::class.java)
+                    .map { ShipmentResponse.transform(it) }
+                _shipments.value = shipmentList
+            } else {
+                _shipments.value = emptyList()
+            }
         }
         return shipments
     }
