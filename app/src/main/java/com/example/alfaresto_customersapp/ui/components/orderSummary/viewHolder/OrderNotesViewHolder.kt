64125dbf3242c308e1f00b5801670b63c96911cb
kelshaaira <kelshairaa@gmail.com>
2024-07-11 11:05:45 +0700

    hide keyboard

--- app/src/main/java/com/example/alfaresto_customersapp/ui/components/orderSummary/viewHolder/OrderNotesViewHolder.kt
+++ app/src/main/java/com/example/alfaresto_customersapp/ui/components/orderSummary/viewHolder/OrderNotesViewHolder.kt
@@ -1,5 +1,7 @@
 package com.example.alfaresto_customersapp.ui.components.orderSummary.viewHolder
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.alfaresto_customersapp.databinding.OrderSummaryNotesBinding
 import com.example.alfaresto_customersapp.ui.components.listener.OrderSummaryItemListener
@@ -21,12 +23,18 @@ class OrderNotesViewHolder(
 //                }
 //            }
 
-            etNotes.setOnEditorActionListener { _, _, _ ->
-                val etNotesText = etNotes.text.toString()
-                Timber.tag("notes vh").d("Notes: $etNotesText")
-                itemListener?.onNotesFilled(etNotesText)
-                etNotes.clearFocus()
-                true
+            etNotes.apply {
+                setOnEditorActionListener { _, _, _ ->
+                    val etNotesText = etNotes.text.toString()
+                    Timber.tag("notes vh").d("Notes: $etNotesText")
+                    itemListener?.onNotesFilled(etNotesText)
+
+                    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(windowToken, 0)
+
+                    etNotes.clearFocus()
+                    true
+                }
             }
         }
     }
