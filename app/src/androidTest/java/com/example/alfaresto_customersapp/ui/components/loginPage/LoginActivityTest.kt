bfd852eb8ceee29d7aacb707be15fcdeea48d38c
abrahamrumondor <intern.abraham@gli.id>
2024-11-19 15:04:14 +0700

    update to make eror

--- app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/loginPage/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/loginPage/LoginActivityTest.kt
@@ -55,16 +55,16 @@ class LoginActivityTest {
 //    onView(withText("Welcome")).check(matches(isDisplayed()))
 
   }
-//
-//  @Test
-//  fun testLoginEmptyFields() {
-//    // Click the login button without filling the fields
-//    onView(withId(R.id.btn_login)).perform(click())
-//
-//    // Check if an error message is shown for the email and password
-//    onView(withId(R.id.til_email)).check(matches(withText("Email is required")))
-//    onView(withId(R.id.til_password)).check(matches(withText("Password is required")))
-//  }
+
+  @Test
+  fun testLoginEmptyFields() {
+    // Click the login button without filling the fields
+    onView(withId(R.id.btn_login)).perform(click())
+
+    // Check if an error message is shown for the email and password
+    onView(withId(R.id.til_email)).check(matches(withText("Email is required")))
+    onView(withId(R.id.til_password)).check(matches(withText("Password is required")))
+  }
 
 //  @Test
 //  fun testRegisterRedirect() {
