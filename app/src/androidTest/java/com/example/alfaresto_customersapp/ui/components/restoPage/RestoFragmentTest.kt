a87129117046829bfe04bee1ae04f558862a7e48
abrahamrumondor <intern.abraham@gli.id>
2024-12-05 15:00:06 +0700

    test hooker ke-22

--- app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/restoPage/RestoFragmentTest.kt
+++ app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/restoPage/RestoFragmentTest.kt
@@ -61,6 +61,8 @@ class RestoFragmentTest {
     MockKAnnotations.init(this)
   }
 
+
+
   private fun setUpMockData() {
     // This can be used to setup any additional mock data
     //    // Mock the ViewModel methods
