e7dd9205cc33546fab0be11c1cc6dac42ad36074
abrahamrumondor <intern.abraham@gli.id>
2024-12-05 15:35:55 +0700

    try trigger #9

--- app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/AddItemTest.kt
+++ app/src/androidTest/java/com/example/alfaresto_customersapp/ui/components/AddItemTest.kt
@@ -59,6 +59,8 @@ class AddItemTest {
 //  var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
 
 
+
+
   @get:Rule
   val mGrantPermissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
     GrantPermissionRule.grant(Manifest.permission.INTERNET)
